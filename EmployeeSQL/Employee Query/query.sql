2f469f044711b060203dd08037a0a61d2813f01b
Riley Furr <jrfurr7@gmail.com>
2021-08-05 11:26:07 -0400

    Adjusting query.sql script

--- EmployeeSQL/Employee Query/query.sql
+++ EmployeeSQL/Employee Query/query.sql
@@ -1,65 +1,66 @@
---Data Engineering
+--Data Engineering--
 
 
--- CREATE TABLE titles(
--- 	title_id VARCHAR PRIMARY KEY
--- 	,title VARCHAR NOT NULL
--- );
+CREATE TABLE titles(
+	title_id VARCHAR PRIMARY KEY
+	,title VARCHAR NOT NULL
+);
 
 
 
--- CREATE TABLE departments(
--- 	dept_no VARCHAR PRIMARY KEY
--- 	,dept_name VARCHAR
--- );
+CREATE TABLE departments(
+	dept_no VARCHAR PRIMARY KEY
+	,dept_name VARCHAR
+);
 
--- CREATE TABLE employees(
--- 	emp_no INT PRIMARY KEY
--- 	,emp_title_id VARCHAR NOT NULL
--- 	,birth_date VARCHAR NOT NULL
--- 	,first_name VARCHAR NOT NULL
--- 	,last_name VARCHAR NOT NULL
--- 	,sex VARCHAR NOT NULL
--- 	,hire_date VARCHAR NOT NULL
--- 	,FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
--- );
+CREATE TABLE employees(
+	emp_no INT PRIMARY KEY
+	,emp_title_id VARCHAR NOT NULL
+	,birth_date VARCHAR NOT NULL
+	,first_name VARCHAR NOT NULL
+	,last_name VARCHAR NOT NULL
+	,sex VARCHAR NOT NULL
+	,hire_date VARCHAR NOT NULL
+	,FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+);
 
--- CREATE TABLE dept_emp(
--- 	emp_no INT NOT NULL
--- 	,dept_no VARCHAR NOT NULL
--- 	,PRIMARY KEY (emp_no, dept_no)
--- 	,FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
--- 	,FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
--- );
+CREATE TABLE dept_emp(
+	emp_no INT NOT NULL
+	,dept_no VARCHAR NOT NULL
+	,PRIMARY KEY (emp_no, dept_no)
+	,FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+	,FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
 
--- CREATE TABLE salaries(
--- 	emp_no INT PRIMARY KEY
--- 	,salary INT NOT NULL
--- 	,FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
--- );
+CREATE TABLE salaries(
+	emp_no INT PRIMARY KEY
+	,salary INT NOT NULL
+	,FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
 
--- CREATE TABLE dept_manager(
--- 	dept_no VARCHAR NOT NULL
--- 	,emp_no INT NOT NULL
--- 	,PRIMARY KEY (dept_no, emp_no)
--- 	,FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
--- 	,FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
--- );
+CREATE TABLE dept_manager(
+	dept_no VARCHAR NOT NULL
+	,emp_no INT NOT NULL
+	,PRIMARY KEY (dept_no, emp_no)
+	,FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+	,FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
 
 --VERIFY DATA IMPORTS--
 
--- SELECT * FROM  titles;
+SELECT * FROM  titles;
 
--- SELECT * FROM salaries;
+SELECT * FROM salaries;
 
--- SELECT * FROM employees;
+SELECT * FROM employees;
 
--- SELECT * FROM dept_manager;
+SELECT * FROM dept_manager;
 
--- SELECT * FROM dept_emp;
+SELECT * FROM dept_emp;
 
--- SELECT * FROM departments;
+SELECT * FROM departments;
 
+--DB Analysis--
 --QUERY FROM DB--
 
 -- 1) List details of each employee: employee #, last name, first name, sex, and salary
