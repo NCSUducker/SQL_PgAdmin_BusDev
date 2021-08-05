2752406ced3f4396203d0c281462b021f1c53385
Riley Furr <jrfurr7@gmail.com>
2021-08-05 11:06:26 -0400

    Adjusting text

--- EmployeeSQL/Employee Query/query.sql
+++ EmployeeSQL/Employee Query/query.sql
@@ -82,8 +82,7 @@ SELECT first_name
 FROM employees
 WHERE hire_date LIKE '%1986';
 
--- 3) Manager of each department with the following: department number, department name,
---													the manager's employee number, last name, first name
+-- 3) Manager of each department with the following: department number, department name, the manager's employee number, last name, first name
 
 SELECT dept_manager.dept_no
 , departments.dept_name
@@ -94,9 +93,7 @@ FROM dept_manager
 JOIN departments ON dept_manager.dept_no = dept_manager.dept_no
 JOIN employees ON dept_manager.emp_no = employees.emp_no;
 
--- 4) List the department of each employee with the following information: employee number,
---																		last name, first name,
---																		and department name
+-- 4) List the department of each employee with the following information: employee number, last name, first name, and department name
 
 SELECT employees.emp_no
 , employees.last_name
@@ -106,8 +103,7 @@ FROM employees
 JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
 JOIN departments ON dept_emp.dept_no = departments.dept_no;
 
--- 5) List first, last name, and sex for employees with the first name 'Hercules' and
---last names begin with the letter 'B'
+-- 5) List first, last name, and sex for employees with the first name 'Hercules' and last names begin with the letter 'B'
 
 SELECT first_name
 , last_name
@@ -115,8 +111,7 @@ SELECT first_name
 FROM employees
 WHERE first_name = 'Hercules' and last_name LIKE 'B%';
 
--- 6) List all employees in the Sales department, including their employee number,
---														last name, first name, and department name
+-- 6) List all employees in the Sales department, including their employee number, last name, first name, and department name
 SELECT * FROM departments;
 --d007 = Sales department
 
@@ -129,8 +124,7 @@ JOIN dept_emp ON  employees.emp_no = dept_emp.emp_no
 JOIN departments ON dept_emp.dept_no = departments.dept_no
 WHERE departments.dept_no = 'd007';
 
---List all employees in the Sales and Development departments,
---including their employee number, last name, first name, and department name
+--List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name
 
 SELECT * FROM departments;
 --d005 = Development department
